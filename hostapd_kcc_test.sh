9909157fe83d1a4a51e9d893cb854d6e1005379a
Timothy Swan <timswan@protonmail.ch>
2017-11-22 16:41:09 -0600

    Fixed bug in FFmpeg script. Added hostapd script.

---
+++ hostapd_kcc_test.sh
@@ -0,0 +1,16 @@
+rm -rf hostapd_kcc_test
+mkdir hostapd_kcc_test
+cd hostapd_kcc_test
+STRTDIR=$(pwd)
+git clone git://w1.fi/hostap.git
+cd hostap
+git checkout af872d9d88b1c868279983ef2c1b65ff81d7347b
+cd hostapd
+export CC=kcc
+export CFLAGS="-std=gnu11 -frecover-all-errors"
+cp defconfig .config
+sed -i '/CONFIG_LIBNL32=y/s/^#//g' .config
+make |& tee kcc_make_out.txt
+cd $STRTDIR
+#sed -i '/<pattern>/s/^#//g' file
+#https://askubuntu.com/questions/617973/fatal-error-netlink-genl-genl-h-no-such-file-or-directory
