164bb00c03f40b62928bb7fc04f88ec6ecea24a4
Timothy Swan <timswan@protonmail.ch>
2017-11-22 13:11:19 -0600

    Changed scripts to adapt with change from config to kcc_config; added FFmpeg and netdata scripts.

---
+++ netdata_kcc_test.sh
@@ -0,0 +1,25 @@
+rm -rf netdata_kcc_test/
+mkdir netdata_kcc_test/
+cd netdata_kcc_test/
+STRTDIR=$(pwd)
+git clone https://github.com/firehol/netdata.git
+cd netdata/
+git checkout 2bbde1ba150b0d6fb66cd01d94b26da0181fd45e
+autoscan
+aclocal
+autoheader
+autoreconf
+automake --add-missing
+autoreconf
+./configure CC=kcc LD=kcc |& tee kcc_configure_out.txt
+make |& tee kcc_make_out.txt
+cd $STRTDIR/netdata/src/
+kcc -d -g -pthread -o apps.plugin apps_plugin.o avl.o clocks.o common.o log.o procfile.o web_buffer.o -lm
+cd $STRTDIR/netdata/
+mkdir kcc_compile_out
+mv kcc_configure_out.txt kcc_compile_out/
+mv kcc_make_out.txt kcc_compile_out/
+mv src/kcc_config kcc_compile_out/
+cd $STRTDIR
+mv $STRTDIR/netdata/kcc_compile_out/ .
+tar -czvf kcc_compile_out.tar.gz kcc_compile_out
