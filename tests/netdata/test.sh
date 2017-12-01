68bd21b1fa8642407c7913f74fe6d28dd7e04685
Timothy Swan <timswan@protonmail.ch>
2017-12-01 13:27:09 -0600

    Removed some extra files. Updates to netdata to integrate it with the new folder structure.

--- tests/netdata/test.sh
+++ tests/netdata/test.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 [ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/TimJSwan89/rv-match_testing/master/prepare.sh
 base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh
-git clone https://github.com/firehol/netdata.git
-build_dir=$base_dir/netdata/
-cd $build_dir
-git checkout 2bbde1ba150b0d6fb66cd01d94b26da0181fd45e
-autoscan
-aclocal
-autoheader
-autoreconf
-automake --add-missing
-autoreconf
-set -o pipefail
-./configure CC=kcc LD=kcc |& tee kcc_configure_out.txt ; configure_success="$?"
-make |& tee kcc_make_out.txt ; make_success="$?"
+_download() {
+    git clone https://github.com/firehol/netdata.git
+    cd netdata/
+    git checkout 2bbde1ba150b0d6fb66cd01d94b26da0181fd45e
+}
+_build() {
+    cd netdata/
+    autoscan
+    aclocal
+    autoheader
+    autoreconf
+    automake --add-missing
+    autoreconf
+    set -o pipefail
+    ./configure CC=kcc LD=kcc |& tee kcc_configure_out.txt ; configure_success="$?"
+    make |& tee kcc_make_out.txt ; make_success="$?"
+}
+init
 cd $build_dir/src/
 kcc -d -g -pthread -o apps.plugin apps_plugin.o avl.o clocks.o common.o log.o procfile.o web_buffer.o -lm |& tee kcc_out.txt
 cd $build_dir/netdata/
