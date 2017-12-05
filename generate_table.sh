672b49cb44b12774e3511ffa82f60d2ef845be4a
Timothy Swan <timswan@protonmail.ch>
2017-12-05 14:33:11 -0600

    Pre-test generate-table 2.0.

--- generate_table.sh
+++ generate_table.sh
@@ -1,5 +1,5 @@
 # To activate ssh key: git remote set-url origin git@github.com:TimJSwan89/rv-match_testing.wiki.git
-tests_dir=$(pwd)
+tests_dir="$(pwd)/tests/"
 git clone https://github.com/TimJSwan89/rv-match_testing.wiki.git
 cd rv-match_testing.wiki
 git pull
@@ -13,46 +13,42 @@ echo "This is an auto generated wiki." > $tablefile
 echo "  " >> $tablefile
 echo "| project | configure gcc | make gcc | configure kcc | make kcc | open issues | closed issues | " >> $tablefile
 echo "| --- | --- | --- | --- | --- | --- | --- |" >> $tablefile
-for file_path in $(ls $tests_dir/*_kcc_test.sh | sort)
-do
-    script_name=$(basename $file_path)
-    test_name=${script_name%_kcc_test.sh}
-    folder_name=$test_name"_kcc_test"
-    
-    # Get configure results
-    configure_result=":grey_question:"
-    configure_result_path=$tests_dir/$folder_name/kcc_compile_out/kcc_configure_success.ini
-    echo $configure_result_path
-    echo -e $configure_result_path
-    if [[ -e $configure_result_path ]] ; then
-        if [[ "$(head -n 1 $configure_result_path)" == 0 ]] ; then
-            configure_result=":white_check_mark:"
+get_successes_for_compiler_and_buildstage() {
+    # Gets either configure or make results from either gcc or kcc
+    result=":grey_question:"
+    result_path=$tests_dir/$test_name/$compiler/log/latest/${buildstage}_success.ini
+    echo $result_path
+    echo -e $result_path
+    if [[ -e $result_path ]] ; then
+        if [[ "$(head -n 1 $result_path)" == 0 ]] ; then
+            result=":white_check_mark:"
         else
-            configure_result=":x:"
+            result=":x:"
         fi
         echo "Test start"
-        echo "$(head -n 1 $configure_result_path)"
+        echo "$(head -n 1 $result_path)"
         echo "Test end"
     fi
+}
+for file_path in $(ls $tests_dir/*/test.sh | sort)
+do
+    test_name=$(basename $(dirname $file_path))
+   
+    compiler="gcc" ; buildstage="configure"
+    gcc_configure_result=get_successes_for_compiler_and_buildstage
+    compiler="gcc" ; buildstage="make"
+    gcc_make_result=get_successes_for_compiler_and_buildstage
+    compiler="kcc" ; buildstage="configure"
+    kcc_configure_result=get_successes_for_compiler_and_buildstage
+    compiler="kcc" ; buildstage="make"
+    kcc_make_result=get_successes_for_compiler_and_buildstage
 
-    # Get make results
-    make_result=":grey_question:"
-    make_result_path=$tests_dir/$folder_name/kcc_compile_out/kcc_make_success.ini
-    echo $make_result_path
-    echo -e $make_result_path
-    if [[ -e $make_result_path ]] ; then
-        if [[ "$(head -n 1 $make_result_path)" == 0 ]] ; then
-            make_result=":white_check_mark:"
-        else
-            make_result=":x:"
-        fi
-    fi
-    
+
     # Get issue {link, number}s in format: [number](link)
     echo "https://api.github.com/search/issues?q=repo:runtimeverification/rv-match+in:title+state:open+$test_name"
     open_issues=$(curl -u "TimJSwan89:$password" "https://api.github.com/search/issues?q=repo:runtimeverification/rv-match+in:title+state:open+$test_name" | jq '[.items[] | {html_url: .html_url, state: .state, number: .number, title: .title}]' | jq -r ".[] | select(.title | contains(\"$test_name\")) | select(.state | contains(\"open\")) | @text \"[\\(.number)](\\(.html_url))\"" | tr '\n' ' ')
     closed_issues=$(curl -u "TimJSwan89:$password" "https://api.github.com/search/issues?q=repo:runtimeverification/rv-match+in:title+state:closed+$test_name" | jq '[.items[] | {html_url: .html_url, state: .state, number: .number, title: .title}]' | jq -r ".[] | select(.title | contains(\"$test_name\")) | select(.state | contains(\"closed\")) | @text \"[\\(.number)](\\(.html_url))\"" | tr '\n' ' ')
-    echo "| $test_name | $configure_result | $make_result | $open_issues | $closed_issues |" >> $tablefile
+    echo "| $test_name | $gcc_configure_result | $gcc_make_result | $kcc_configure_result | $kcc_make_result | $open_issues | $closed_issues |" >> $tablefile
 done
 #rm rv-match_issues.json
 
@@ -60,7 +56,7 @@ done
 echo "  " >> $tablefile
 echo "| project | standalone script |  " >> $tablefile
 echo "| --- | --- |  " >> $tablefile
-for file_path in $(ls $tests_dir/*_kcc_test.sh | sort)
+for file_path in $(ls $tests_dir/*/test.sh | sort)
 do
     script_name=$(basename $file_path)
     test_name=${script_name%_kcc_test.sh}
