722543eab995f9c689b646cbccbbeacac84a834c
Timothy Swan <timswan@protonmail.ch>
2017-12-07 11:17:00 -0600

    Updated run_all script to use the run_set script and exclude tests that crash the machine.

--- run_all.sh
+++ run_all.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-for x in tests/*/test.sh
-do
-    echo ==== $x started at $(date)
-    bash $x
-    echo ==== $x finished at $(date)
-done
-bash extract_output.sh
+
+bash run_set.sh sets/crashless.ini
+
+#for x in tests/*/test.sh
+#do
+#    echo ==== $x started at $(date)
+#    bash $x
+#    echo ==== $x finished at $(date)
+#done
+#bash extract_output.sh
 
 # k-bin-to-text kcc_config kcc_config.txt
 # sed 's/<k>/\n<k>/' kcc_config.txt | tail -n1 >kcc_k_config.txt
