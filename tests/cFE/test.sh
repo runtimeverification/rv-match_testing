1027eef638cc96f73e04423ae9420fbc785f3944
Timothy Swan <timswan@protonmail.ch>
2017-12-07 17:11:50 -0600

    cFE build updates.

--- tests/cFE/test.sh
+++ tests/cFE/test.sh
@@ -5,25 +5,23 @@ base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh
 _download() {
     wget "https://sourceforge.net/projects/coreflightexec/files/cFE-6.5.0a-OSS-release.tar.gz"
     tar -xvzf cFE-6.5.0a-OSS-release.tar.gz
-    cd cFE-6.5.0-OSS-release/
-    rm -rf osal/
-    git clone https://github.com/nasa/osal.git
-    cd osal
-    git checkout 7139592f04e47f7522b07b1ef9f84a21393df88a
-    cd src/os
+    wget "https://sourceforge.net/projects/osal/files/osal-4.2.1a-release.tar.gz"
+    tar zxf osal-4.2.1a-release.tar.gz
+    cd osal-4.2.1a-release/src/os/
     ln -sf posix posix-ng
-    cd $download_dir/cFE-6.5.0-OSS-release/
-    cp -r cfe/cmake/sample_defs/ cfe/sample_defs/
+    cd ../../../cFE-6.5.0-OSS-release/
+    rm -r osal
+    ln -s ../osal-4.2.1a-release ./osal
     rm -r build/
     mkdir build/
-    cp $download_dir/cFE.m64.patch .
-    find . -type f \! -name '*_log.txt' \! -name '*.doc' -print0 | xargs -0 grep -l -- -m32
+    # sudo apt install libc6-dev-i386
+    # Refer to the other files for getting rvpc to work
 }
 
 _build() {
-    cd cFE-6.5.0-OSS-release/
+    cd cFE-6.5.0-OSS-release/build/
     export SIMULATION=native
-    cmake -DCMAKE_C_COMPILER=$compiler -DENABLE_UNIT_TESTS=TRUE --build ; configure_success="$?"
+    cmake -DCMAKE_C_COMPILER=$compiler -DENABLE_UNIT_TESTS=TRUE --build ../cfe ; configure_success="$?"
     make mission-all ; make_success="$?"
 }
 
@@ -32,19 +30,3 @@ _extract() {
 }
 
 init
-
-#you start by placing the osal directory under "osal" under the cfe directory
-#for some reason it wants the os-specific code for the posix-ng platform which do
-#esn't appear to be included in the repository
-#so I just created a symlink from posix-ng to posix
-#in osal/src/os
-#then you copy cfe/cmake/sample_defs to sample_defs under the cfe root
-#then you make a directory build-sim
-#cd to it
-#export SIMULATION=native
-#and run cmake -DENABLE_UNIT_TESTS=TRUE ../cfe
-#followed by make mission-all to build all the tests
-#and then you cd into the native directory and run ctest
-#that will build it for gcc
-#so all you need to do to build it for rv-predict is
-#add the -DCMAKE_C_COMPILER flag
