2dbb3175f3513fb9f64c29425fbfe2b707935dcb
Timothy Swan <timswan@protonmail.ch>
2017-12-14 12:16:44 -0600

    Spin build configs

--- tests/spin/test.sh
+++ tests/spin/test.sh
@@ -11,8 +11,8 @@ _download() {
 
 _build() {
     cd Spin/Src6.4.7
-    sed -i -e "s/CC=gcc/CC=$compiler/g" makefile ; configure_success="$?"
-    #sed -i -e "s/CC=gcc/CC=$compiler -frecover-all-errors/g" makefile ; configure_success="$?"
+    #sed -i -e "s/CC=gcc/CC=$compiler/g" makefile ; configure_success="$?"
+    sed -i -e "s/CC=gcc/CC=$compiler -frecover-all-errors/g" makefile ; configure_success="$?"
     make ; make_success="$?"
     echo "sdufhod"
 }
