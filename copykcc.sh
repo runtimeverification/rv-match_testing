7c1cd92abfae95b06d01e4b4a793777302c118b0
Timothy Swan <timswan@protonmail.ch>
2017-12-18 20:46:45 -0600

    Adding debug statements.

--- copykcc.sh
+++ copykcc.sh
@@ -5,4 +5,9 @@ rm -r dep3/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/c-semantics/dist ./dep1/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/errors/rv-error/bin ./dep2/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/k/k-distribution/target/release/k/bin ./dep3/
+cd dep1/ ; ls ; cd ..
+cd dep2/ ; ls ; cd ..
+cd dep3/ ; ls ; cd ..
+echo "Old PATH: "$PATH
 PATH=$(pwd)/dep1:$(pwd)/dep2:$(pwd)/dep3:$PATH
+echo "New PATH: "$PATH
