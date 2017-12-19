c7da832087133acd61261c2dfcd732a05f8adc09
Timothy Swan <timswan@protonmail.ch>
2017-12-18 20:43:04 -0600

    Prefixing PATH instead of postfixing.

--- copykcc.sh
+++ copykcc.sh
@@ -5,4 +5,4 @@ rm -r dep3/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/c-semantics/dist ./dep1/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/errors/rv-error/bin ./dep2/
 cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/k/k-distribution/target/release/k/bin ./dep3/
-PATH=$PATH:$(pwd)/dep1:$(pwd)/dep2:$(pwd)/dep3
+PATH=$(pwd)/dep1:$(pwd)/dep2:$(pwd)/dep3:$PATH
