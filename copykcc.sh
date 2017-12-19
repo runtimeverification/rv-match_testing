e749146b1ccb316375a02737556f6b3a21dc2d82
Timothy Swan <timswan@protonmail.ch>
2017-12-18 20:19:19 -0600

    Recursive copy.

--- copykcc.sh
+++ copykcc.sh
@@ -2,7 +2,7 @@
 rm -r dep1/
 rm -r dep2/
 rm -r dep3/
-cp /var/lib/jenkins/jobs/rv-match-master/workspace/c-semantics/dist ./dep1/
-cp /var/lib/jenkins/jobs/rv-match-master/workspace/errors/rv-error/bin ./dep2/
-cp /var/lib/jenkins/jobs/rv-match-master/workspace/k/k-distribution/target/release/k/bin ./dep3/
+cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/c-semantics/dist ./dep1/
+cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/errors/rv-error/bin ./dep2/
+cp -r /var/lib/jenkins/jobs/rv-match-master/workspace/k/k-distribution/target/release/k/bin ./dep3/
 PATH=$PATH:$(pwd)/dep1:$(pwd)/dep2:$(pwd)/dep3
