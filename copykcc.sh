196682effcb49366deca12335d66c40eaac6926e
Timothy Swan <timswan@protonmail.ch>
2017-12-18 20:17:47 -0600

    Added a script to copy kcc and it's dependencies and add them to the PATH variable.

---
+++ copykcc.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -r dep1/
+rm -r dep2/
+rm -r dep3/
+cp /var/lib/jenkins/jobs/rv-match-master/workspace/c-semantics/dist ./dep1/
+cp /var/lib/jenkins/jobs/rv-match-master/workspace/errors/rv-error/bin ./dep2/
+cp /var/lib/jenkins/jobs/rv-match-master/workspace/k/k-distribution/target/release/k/bin ./dep3/
+PATH=$PATH:$(pwd)/dep1:$(pwd)/dep2:$(pwd)/dep3
