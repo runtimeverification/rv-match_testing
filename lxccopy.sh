fea51c245718ffffcb9ace90e313537bcecd4e8d
Timothy Swan <timswan@protonmail.ch>
2017-12-20 15:09:11 -0600

    Added script to use LXC.

---
+++ lxccopy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+function stopLxc {
+  lxc-stop -n rv-match_projtesting_container
+}
+unset XDG_SESSION_ID
+unset XDG_RUNTIME_DIR
+unset XDG_SESSION_COOKIE
+lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins_:ro -n ubuntu-14.04-java -N rv-match_projtesting_container \
+&& trap stopLxc EXIT
+lxc-attach -n rv-match_projtesting_container -- su -l -c /mnt/jenkins_/rv-match_testing/echohello.sh
