9c419f9d94332427a2d42f734a6ed76deb66239b
Timothy Swan <timswan@protonmail.ch>
2017-12-20 23:44:15 -0600

    Debugging virtual filepath containing host.

--- echohello.sh
+++ echohello.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 echo "Hello, world from "$(pwd)
 mkdir -p tests/helloworld/
-cp /mnt/jenkins/tests/helloworld/test.sh /tests/helloworld/test.sh
+ls /mnt/jenkins/
+cd /mnt/jenkins/
 ls
+cp tests/helloworld/test.sh ../../tests/helloworld/test.sh
+ls
+cd ../..
 bash tests/helloworld/test.sh
