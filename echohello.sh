8ab5d8af73df565bf533b196fd0ab6c9ab5b7207
Timothy Swan <timswan@protonmail.ch>
2017-12-21 11:46:31 -0600

    Forgot sh.

--- echohello.sh
+++ echohello.sh
@@ -3,26 +3,15 @@ echo "Hello, world from "$(pwd)
 ls
 mkdir -p tests/helloworld/
 touch tests/helloworld/test.sh
-ls
-ls tests/helloworld/
-ls /mnt/jenkins/
-echo "pwd is "$(pwd)
 cd /mnt/jenkins/
-echo "pwd is "$(pwd)
-ls
 cp tests/helloworld/test.sh /root/tests/helloworld/test.sh
 cp run_set.sh /root/run_set.sh
 mkdir /root/sets/
 cp -r /sets/* /root/sets/
+echo " === These are what is in the sets directory: "
+ls /root/sets/
 cp prepare.sh /root/prepare.sh
-ls
-echo "huh?"
-ls ../../root/
-echo "pwd is "$(pwd)
-echo "uh."
-ls
 cd /root/
-echo "pwd is "$(pwd)
-echo "hrm"
-ls
+echo " == Should be the same "
+ls /sets/
 bash run_set.sh sets/quickset.ini
