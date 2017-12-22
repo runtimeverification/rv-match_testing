7a01b7eae0340ff11d09e719b83b29fa80922a87
Timothy Swan <timswan@protonmail.ch>
2017-12-22 12:34:31 -0600

    Cleaning up the copykcc script a bit.

--- copykcc.sh
+++ copykcc.sh
@@ -3,7 +3,6 @@ currentdir=$(pwd)
 kccsource="/var/lib/jenkins/jobs/rv-match-master/workspace"
 cd $kccsource
 kccgithash=$(git rev-parse HEAD)
-echo $kccgithash
 cd $currentdir
 rm -r kcc_dependency_1/
 rm -r kcc_dependency_2/
@@ -13,13 +12,14 @@ cp -r $kccsource/errors/rv-error/bin ./kcc_dependency_2/
 cp -r $kccsource/k/k-distribution/target/release/k/bin ./kcc_dependency_3/
 cd $kccsource
 kccgithash2=$(git rev-parse HEAD)
-echo "first hash "$kccgithash
-echo "next  hash "$kccgithash2
+echo "rv-match git hash before copying kcc: "$kccgithash
+echo "rv-match git hash after  copying kcc: "$kccgithash2
 if [[ $kccgithash != $kccgithash2 ]] ; then
     echo "Warning: git hash changed during copy."
 else
     echo "Git hash remained the same during copy."
 fi
+echo "Found by running \"git rev-parse HEAD\" in \"$kccsource\".
 cd $currentdir
 cd kcc_dependency_1/ ; ls ; cd ..
 cd kcc_dependency_2/ ; ls ; cd ..
