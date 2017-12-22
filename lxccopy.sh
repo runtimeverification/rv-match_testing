1065ebbfd3246c87ab3aff625aa6d28ab1a378b8
Timothy Swan <timswan@protonmail.ch>
2017-12-22 16:22:08 -0600

    Testing LXC network info commands.

--- lxccopy.sh
+++ lxccopy.sh
@@ -8,6 +8,10 @@ unset XDG_RUNTIME_DIR
 unset XDG_SESSION_COOKIE
 lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n ubuntu-14.04-java -N rv-match_projtesting_container \
 && trap stopLxc EXIT
+lxc-info --name rv-match_projtesting_container
+lxc network create testbr0
+lxc network show testbr0
+lxc network attach testbr0 rv-match_projtesting_container default eth0
 lxc-info --name ubuntu-14.04-java
 lxc-info --name rv-match_projtesting_container
 lxc-attach -n rv-match_projtesting_container -- su -l -c /mnt/jenkins/echohello.sh
