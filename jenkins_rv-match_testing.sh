1773aaed365d1f6d344b15f16b1967cd5a63faa1
Timothy Swan <timswan@protonmail.ch>
2017-12-22 14:45:53 -0600

    Trying out running a top level script, but I suspect issues with exporting variables.

---
+++ jenkins_rv-match_testing.sh
@@ -0,0 +1,10 @@
+sh copykcc.sh
+export PATH=$(pwd)/kcc_dependency_1:$(pwd)/kcc_dependency_2:$(pwd)/kcc_dependency_3:$PATH
+echo "PATH variable contents: "$PATH
+echo "kcc --version"
+kcc --version
+echo "kcc -version"
+kcc -version
+echo "which kcc"
+which kcc
+bash run_regression_set.sh sets/quickset.ini
