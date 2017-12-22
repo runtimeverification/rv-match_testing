0eff7334a3ba535947fdfa0dafbb4a9e6b04a31c
Timothy Swan <timswan@protonmail.ch>
2017-12-22 16:48:38 -0600

    Getting more info on lxc for online help.

--- lxccopy.sh
+++ lxccopy.sh
@@ -9,9 +9,11 @@ unset XDG_SESSION_COOKIE
 lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n ubuntu-14.04-java -N rv-match_projtesting_container \
 && trap stopLxc EXIT
 lxc-info --name rv-match_projtesting_container
-lxc network create testbr0
-lxc network show testbr0
-lxc network attach testbr0 rv-match_projtesting_container default eth0
+lxc-start --version
+lxc-checkconfig
+uname -a
+cat /proc/self/cgroup
+cat /proc/1/mounts
 lxc-info --name ubuntu-14.04-java
 lxc-info --name rv-match_projtesting_container
 lxc-attach -n rv-match_projtesting_container -- su -l -c /mnt/jenkins/echohello.sh
