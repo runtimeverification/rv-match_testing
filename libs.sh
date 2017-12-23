f475e3388f2bb52c266aaaca06b293bcc8374da5
Timothy Swan <timswan@protonmail.ch>
2017-12-22 19:54:20 -0600

    apt install debugs

--- libs.sh
+++ libs.sh
@@ -1,4 +1,6 @@
-# helloworld
+printheader="In libs.sh section: "
+
+echo $printheader"helloworld"
 sudo apt-get update
 sudo apt -y upgrade
 sudo apt -y install gcc
@@ -6,9 +8,17 @@ sudo apt -y install build-essential
 sudo apt -y install perl
 sudo apt -y install git-all
 
-# prepare.sh
+echo $printheader"prepare.sh"
 sudo apt -y install bc
 
-# bogosort
+echo $printheader"bogosort"
 which cpan
 cpan String::Escape
+cpan Getopt::Declare
+
+echo $printheader"Reptile"
+sudo apt install linux-generic
+
+echo $printheader"[For any remaining apt kinks...]"
+sudo apt update
+sudo apt -y upgrade
