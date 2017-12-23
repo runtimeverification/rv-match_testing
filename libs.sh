10d456278daeed33d81947acbbbd330b522d238d
Timothy Swan <timswan@protonmail.ch>
2017-12-22 19:33:23 -0600

    apt install debugs

--- libs.sh
+++ libs.sh
@@ -4,6 +4,11 @@ sudo apt -y upgrade
 sudo apt -y install gcc
 sudo apt -y install build-essential
 sudo apt -y install perl
+sudo apt -y install git-all
 
 # prepare.sh
-sudp apt -y install bc
+sudo apt -y install bc
+
+# bogosort
+which cpan
+cpan String::Escape
