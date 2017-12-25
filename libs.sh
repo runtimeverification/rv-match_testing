e9b2045345f2b63c3295e2355c782caeb676f357
Timothy Swan <timswan@protonmail.ch>
2017-12-24 23:50:50 -0600

    Cleaning up project run and testing kcc on getty

--- libs.sh
+++ libs.sh
@@ -3,6 +3,7 @@ printheader="In libs.sh section: "
 echo $printheader"helloworld"
 sudo apt-get update
 sudo apt -y upgrade
+sudo apt -y install bash
 sudo apt -y install gcc
 sudo apt -y install build-essential
 sudo apt -y install perl
