771587e77c50e07053485bc146a01562a4b619b8
Timothy Swan <timswan@protonmail.ch>
2018-01-03 15:58:03 -0600

    Removed lxccopy.sh and lxccopy_regression.sh and replaced with container_run.sh

--- container_run.sh
+++ container_run.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
+currentscript="container_run.sh"
+container="rv-match_projtesting_container"
+source_container="ubuntu-14.04-java"
+guest_script="echohello.sh"
+while getopts ":r" opt; do
+  case ${opt} in
+    r ) echo $currentscript" regression option selected."
+        container="rv-match_regression_container"
+        guest_script="echohello_regression.sh"
+      ;;
+    \? ) echo "Usage: cmd [-r]"
+      ;;
+  esac
+done
+
 set -e
 function stopLxc {
-  lxc-stop -n rv-match_projtesting_container
+  lxc-stop -n $container
 }
 unset XDG_SESSION_ID
 unset XDG_RUNTIME_DIR
 unset XDG_SESSION_COOKIE
-#lxc-destroy -f --name rv-match_projtesting_container
-lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n ubuntu-14.04-java -N rv-match_projtesting_container \
+#lxc-destroy -f --name $container
+lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -N $container \
 && trap stopLxc EXIT
-lxc-info --name rv-match_projtesting_container
+lxc-info --name $container
 lxc-start --version
 lxc-checkconfig
 uname -a
 cat /proc/self/cgroup
 cat /proc/1/mounts
-lxc-info --name ubuntu-14.04-java
-echo "Testing container info:"
-lxc-info --name rv-match_projtesting_container
-lxc-start -n rv-match_projtesting_container
-lxc-attach -n rv-match_projtesting_container -- su -l -c /mnt/jenkins/echohello.sh
+lxc-info --name $source_container
+echo "Testing container's info:"
+lxc-info --name $container
+lxc-start -n $container
+lxc-attach -n $container -- su -l -c /mnt/jenkins/$guest_script
