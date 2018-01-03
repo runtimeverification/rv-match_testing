a377e05c33d5bc33f8a0b4db1cd262fa4f5e467d
Timothy Swan <timswan@protonmail.ch>
2018-01-03 17:30:04 -0600

    Had forgotten to pass on regression flag to container script.

--- jenkins_run.sh
+++ jenkins_run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 exportfile="report"
 currentscript="jenkins_run.sh"
+containerscriptflags=""
 while getopts ":r" opt; do
   case ${opt} in
     r ) echo $currentscript" regression option selected."
         exportfile="regression"
+        containerscriptflags=" -r"
       ;;
     \? ) echo "Usage: cmd [-r]"
          echo " -r regression"
@@ -15,4 +17,4 @@ done
 bash copy_kcc_dependencies.sh
 chmod 777 results/$exportfile.xml
 ls -la results/
-bash container_run.sh
+bash container_run.sh$containerscriptflags
