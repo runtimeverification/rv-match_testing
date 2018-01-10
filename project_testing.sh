e0fa057c6b08e4ad6cacbe0234a3604864f14eb9
Timothy Swan <timswan@protonmail.ch>
2018-01-10 16:24:57 -0600

    Got rid of two unnecessary/unused top level scripts.

--- project_testing.sh
+++
@@ -1,8 +0,0 @@
-#wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/project_testing.sh && bash project_testing.sh
-git clone https://github.com/runtimeverification/rv-match_testing.git
-user_dir=$(pwd)
-cd rv-match_testing/
-git pull
-bash run_set.sh sets/quickset.ini
-cd $user_dir
-rm project_testing.sh
