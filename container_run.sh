d0a5ef3259245376543bb022a25cb6583fc33a51
Timothy Swan <timswan@protonmail.ch>
2018-01-12 09:34:01 -0600

    Attempting .config setup for network debug.

--- container_run.sh
+++ container_run.sh
@@ -43,9 +43,15 @@ echo "container: $container"
 if [[ `lxc-ls` == *"rv-match_testing_container"* ]] ; then
     lxc-destroy -f -n rv-match_testing_container
 fi
-#lxc-destroy -f --name "ubuntu-zesty-source-rv-match_testing"
+ls ~/.config/lxc
+ mkdir -p ~/.config/lxc
+ echo "lxc.id_map = u 0 1214112 65536" > ~/.config/lxc/default.conf
+ echo "lxc.id_map = g 0 1214112 65536" >> ~/.config/lxc/default.conf
+ echo "lxc.network.type = veth" >> ~/.config/lxc/default.conf
+ echo "lxc.network.link = lxcbr0" >> ~/.config/lxc/default.conf
+lxc-destroy -f --name "ubuntu-zesty-source-rv-match_testing"
 lxc-checkconfig
-#lxc-create -t download -n $source_container -- -d ubuntu -r zesty -a amd64
+lxc-create -t download -n $source_container -- -d ubuntu -r zesty -a amd64
 lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -N $container \
 && trap stopLxc EXIT
 lxc-checkconfig
