b0e99282c49aab37d758546b3dbc89098a0de74d
Timothy Swan <timswan@protonmail.ch>
2018-01-11 19:28:31 -0600

    Container creation debug.

--- container_run.sh
+++ container_run.sh
@@ -40,9 +40,10 @@ unset XDG_RUNTIME_DIR
 unset XDG_SESSION_COOKIE
 #lxc-destroy -f --name $container
 #lxc-destroy -f --name "ubuntu-zesty-source-rv-match_testing"
-lxc-create -e -D -t download -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -- -d ubuntu -r zesty -a amd64
-lxc-start -n $source_container
-lxc-attach -n $source_container -- su -l -c "/mnt/jenkins/source_guest_setup.sh"
+lxc-create -t download -n $source_container -- -d ubuntu -r zesty -a amd64
+lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -N $container \
+lxc-start -n $container
+lxc-attach -n $container -- su -l -c "/mnt/jenkins/source_guest_setup.sh"
 
 #lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -N $container \
 #&& trap stopLxc EXIT
