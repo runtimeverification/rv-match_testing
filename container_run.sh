d0fb7d05d8832e1db93257fe6286ac1b54ffe108
Timothy Swan <timswan@protonmail.ch>
2018-01-11 18:59:56 -0600

    Trying to set up mount. Trying -m

--- container_run.sh
+++ container_run.sh
@@ -43,7 +43,7 @@ unset XDG_RUNTIME_DIR
 unset XDG_SESSION_COOKIE
 #lxc-destroy -f --name $container
 lxc-destroy -f --name "ubuntu-zesty-source-rv-match_testing"
-lxc-create -t download bind=`pwd`:/mnt/jenkins:rw -n $source_container -- -d ubuntu -r zesty -a amd64
+lxc-create -t download -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -- -d ubuntu -r zesty -a amd64
 lxc-start -n $source_container
 lxc-attach -n $source_container -- su -l -c "/mnt/jenkins/source_guest_setup.sh"
 && trap stopSourceLxc EXIT
