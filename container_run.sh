1b7e1d0ae72d269de5022fa29c81be50805f6942
Timothy Swan <timswan@protonmail.ch>
2018-01-11 20:00:31 -0600

    Run checkconfig before copy. Not sure why network isn't working. Perhaps need to configure?

--- container_run.sh
+++ container_run.sh
@@ -44,6 +44,7 @@ if [[ `lxc-ls` == *"rv-match_testing_container"* ]] ; then
     lxc-destroy -f -n rv-match_testing_container
 fi
 #lxc-destroy -f --name "ubuntu-zesty-source-rv-match_testing"
+lxc-checkconfig
 #lxc-create -t download -n $source_container -- -d ubuntu -r zesty -a amd64
 lxc-copy -s -e -B overlay -m bind=`pwd`:/mnt/jenkins:rw -n $source_container -N $container \
 && trap stopLxc EXIT
