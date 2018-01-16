78cfb77543e3586c307f64277bb0f4af538a7b7f
Timothy Swan <timswan@protonmail.ch>
2018-01-16 17:15:27 -0600

    Testing time it takes to run spin tests with kcc on jenkins.

--- guest_run.sh
+++ guest_run.sh
@@ -75,9 +75,9 @@ mainscript_testing() {
     #bash libs.sh
     #bash tests/getty/test.sh
     #bash merged.sh sets/crashless.ini
-    #bash merged.sh sets/temporary.ini
+    bash merged.sh sets/temporary.ini
     #bash merged.sh sets/interesting.ini
-    bash merged.sh sets/quickset.ini
+    #bash merged.sh sets/quickset.ini
     #cp results/status.xml $hostspace/results/
 }
 mainscript_regression() {
