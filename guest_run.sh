6349b4f7a6d16f7801a09527031225a374dd9592
Timothy Swan <timswan@protonmail.ch>
2018-01-16 13:52:19 -0600

    Looking at workspace inside container.

--- guest_run.sh
+++ guest_run.sh
@@ -66,7 +66,8 @@ echo "</placement debug>"
 
 # Part 3 Run Main Script
 mainscript_testing() {
-    bash unit_test_merged.sh
+    ls -la
+    #bash unit_test_merged.sh
     #bash libs.sh
     #bash tests/getty/test.sh
     #bash merged.sh sets/crashless.ini
