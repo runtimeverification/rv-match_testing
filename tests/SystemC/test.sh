522420e37a7ea1ecedad7a8e6a151981d5de7912
Timothy Swan <timswan@protonmail.ch>
2018-01-17 15:03:59 -0600

    Updated test.sh headers to what prepare.sh forces them to be at runtime anyway. This is so that jenkins runs are not trying to update the git repository.

--- tests/SystemC/test.sh
+++ tests/SystemC/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
 #git clone https://github.com/ArchC/SystemC.git
 #cd SystemC
 #git checkout 778e7c6b45564494a98e6c1526f492b271c5696e
--- tests/systemc-2.3/test.sh
+++ tests/systemc-2.3/test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
 #git clone https://github.com/systemc/systemc-2.3.git
 #cd systemc-2.3/
 #git checkout 686336b03ea17374024df0ae8272d7e22ba2692f
