78151f1a9457a9ed248caa02b452a62c75dea4fe
Timothy Swan <timswan@protonmail.ch>
2018-01-18 11:01:15 -0600

    Added projects {redis, git, luna} and they are set temporary.ini for jenkins testing.

---
+++ tests/redis/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
+_download() {
+    git clone https://github.com/antirez/redis.git
+    cd redis/
+    git checkout 727dd43614ec45e23e2dedbba08b393323feaa4f
+}
+
+_build() {
+    cd redis/ ; configure_success="$?"
+    make CC=$compiler LD=$compiler ; make_success="$?"
+    if [ "$make_success" == "0" ] ; then
+        make test CC=$compiler LD=$compiler ; make_success="$?"
+    fi
+}
+
+_test() {
+    :
+}
+
+init
