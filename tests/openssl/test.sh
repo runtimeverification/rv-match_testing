d91e8324f320da7ab1ebd3478b1ff6264bb1dd3f
Chris Hathhorn <chathhorn@gmail.com>
2018-01-17 22:03:45 -0600

    Try different flags for openssl

--- tests/openssl/test.sh
+++ tests/openssl/test.sh
@@ -12,9 +12,12 @@ _download() {
 _build() {
     cd openssl/
     set -o pipefail
-    CC=$compiler LD=$compiler ./config |& tee kcc_config_out.txt ; configure_success="$?"
+    if [[ $compiler == "kcc" ]]; then
+        CC=kcc CFLAGS="-std=gnu11 -no-pedantic -frecover-all-errors" LD=kcc ./config no-asm no-threads no-hw no-zlib no-shared |& tee kcc_config_out.txt ; configure_success="$?"
+    else
+        CC=$compiler LD=$compiler ./config no-asm no-threads no-hw no-zlib no-shared |& tee kcc_config_out.txt ; configure_success="$?"
+    fi
     make |& tee kcc_make_out.txt ; make_success="$?"
-    $compiler -d -I. -Icrypto/include -Iinclude -DDSO_DLFCN -DHAVE_DLFCN_H -DNDEBUG -DOPENSSL_THREADS -DOPENSSL_NO_STATIC_ENGINE -DOPENSSL_PIC -DOPENSSL_IA32_SSE2 -DOPENSSL_BN_ASM_MONT -DOPENSSL_BN_ASM_MONT5 -DOPENSSL_BN_ASM_GF2m -DSHA1_ASM -DSHA256_ASM -DSHA512_ASM -DRC4_ASM -DMD5_ASM -DAES_ASM -DVPAES_ASM -DBSAES_ASM -DGHASH_ASM -DECP_NISTZ256_ASM -DPADLOCK_ASM -DPOLY1305_ASM -DOPENSSLDIR="/usr/local/ssl" -DENGINESDIR="/usr/local/lib/engines-1.1" -Wall -O3 -pthread -m64 -DL_ENDIAN -fPIC -DOPENSSL_USE_NODELETE -c -o crypto/bio/bss_bio.o crypto/bio/bss_bio.c |& tee kcc_out.txt
 }
 
 init
