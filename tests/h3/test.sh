d1e14c43afd48cdd95b725a2b16a2fc1b9c9a196
Timothy Swan <timswan@protonmail.ch>
2018-01-18 16:04:01 -0600

    Added h3 and set for testing it.

---
+++ tests/h3/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
+_download() {
+    git clone https://github.com/uber/h3.git
+    cd h3/
+    git checkout 8b02a95cd93530cb83ea3bd84a3d60c0cad6c9cd
+}
+
+_build() {
+    cd h3/
+    CC=$compiler LD=$compiler cmake . ; configure_success="$?"
+    make ; make_success="$?"
+}
+
+_test() {
+    :
+}
+
+init
