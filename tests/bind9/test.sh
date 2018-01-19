22db658ede041152d3f4a855f1006cb9e3eb81ef
Chris Hathhorn <chathhorn@gmail.com>
2018-01-19 12:49:39 -0600

    Bind flags

--- tests/bind9/test.sh
+++ tests/bind9/test.sh
@@ -13,7 +13,7 @@ _build() {
     autoreconf
     set -o pipefail
     if [[ $compiler == "kcc" ]]; then
-        ./configure CC=kcc CFLAGS="-std=gnu11 -no-pedantic -frecover-all-errors" LD=kcc |& tee kcc_configure_out.txt ; configure_success="$?"
+        ./configure CC=kcc CFLAGS="-std=gnu11 -no-pedantic -frecover-all-errors" LD=kcc --disable-threads --disable-atomic --disable-shared |& tee kcc_configure_out.txt ; configure_success="$?"
     else
         ./configure CC=$compiler |& tee kcc_configure_out.txt ; configure_success="$?"
     fi
