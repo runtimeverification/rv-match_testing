52d3fc97b01613546923fba49e558ba37aa51f95
Timothy Swan <timswan@protonmail.ch>
2018-02-06 11:44:53 -0600

    Testing self unit tests and minuteset on rvwork-2.

--- guest_run.sh
+++ guest_run.sh
@@ -122,15 +122,15 @@ echo "</k-bin-to-text prep>"
 
 # Part 3 Run Main Script
 mainscript_testing() {
-    #bash unit_test_merged.sh
-     #bash libs.sh
+    bash unit_test_merged.sh
+    bash libs.sh
     #bash tests/getty/test.sh
     #bash merged.sh sets/crashless.ini
-     #bash merged.sh sets/temporary.ini
+    #bash merged.sh sets/temporary.ini
     #bash merged.sh sets/interesting.ini
     #bash merged.sh sets/quickset.ini
     #cp results/status.xml $hostspace/results/
-    bash unit_test_merged.sh
+    bash merged.sh sets/minuteset.ini
 }
 mainscript_regression() {
     bash libs.sh
