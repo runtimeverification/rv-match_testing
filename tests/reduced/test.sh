7e1f40baa29e1427dc916bd3fe4544d034f866ff
Timothy Swan <timswan@protonmail.ch>
2018-02-22 11:44:40 -0600

    Forgot to track test.sh in git.

---
+++ tests/reduced/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
+_dependencies() {
+    :
+}
+
+_download() {
+    if [ -d ../published/ ] && [ ! -d published_copy/ ] ; then
+        cp -r ../published/ published_copy/
+        cd published_copy/
+    fi
+}
+
+_build() {
+    cd published_copy/ ; echo "$?" |& tee kcc_build_0.txt ; results[0]="$?" ; process_kcc_config 0
+    index=1
+    for f in *.c; do
+        names[index]=${f%".c"}
+        $compiler -o ${f%".c"} $f |& tee kcc_build_$index.txt ; results[index]="$?" ; process_kcc_config $index
+        index=$((index+1))
+    done
+}
+
+_test() {
+    cd published_copy/
+    names[0]="kcc -v test"
+    kcc -v |& tee "kcc_out_0.txt" ; results[0]="$?"
+    index=1
+    for f in *.c; do
+        names[index]=${f%".c"}
+        ${f%".c"} |& tee kcc_out_$index.txt ; results[index]="$?" ; process_config
+        index=$((index+1))
+    done
+}
+
+init
