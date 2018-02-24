411511b486882aaaf84e6fcb03242af3523bd55d
Timothy Swan <timswan@protonmail.ch>
2018-02-24 13:31:25 -0600

    LuaDist on jenkins debug.

--- prepare.sh
+++ prepare.sh
@@ -96,6 +96,20 @@ prep_prepare() {
     mkdir -p $unit_test_dir
 
     if [ ! -d $dependency_dir ] || [ -z "$(ls -A $dependency_dir)" ] || [ ! -e $dependency_dir/dependency_function_hash ] || [ "$(echo $(sha1sum <<< $(type _dependencies)))" != "$(head -n 1 $dependency_dir/dependency_function_hash)" ] ; then
+    i=0
+    tput sc
+    while fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
+        case $(($i % 4)) in
+            0 ) j="-" ;;
+            1 ) j="\\" ;;
+            2 ) j="|" ;;
+            3 ) j="/" ;;
+        esac
+        tput rc
+        echo -en "\r[$j] Waiting for other software managers to finish..." 
+        sleep 0.5
+        ((i=i+1))
+    done 
     _dependencies
     echo $report_string" installing dependencies. Either this is the initial installation or the dependency hash has changed since the last install."
         rm $dependency_dir/dependency_function_hash ; rm -r $dependency_dir
