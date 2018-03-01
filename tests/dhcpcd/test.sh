b2f2b5d3c5f82c9af79f79eb5e11a2f53fd7bf6c
Timothy Swan <timswan@protonmail.ch>
2018-03-01 04:58:36 -0600

    Added dhcpcd tests and build step.

--- tests/dhcpcd/test.sh
+++ tests/dhcpcd/test.sh
@@ -19,6 +19,20 @@ _build() {
     set -o pipefail
     make |& tee kcc_build_1.txt
     [ -f src/dhcpcd ] ; results[1]="$?" ; process_kcc_config 1
+
+    # Leave passing regression tests alone.
+    if [ "$exportfile" == "regression" ] ; then return ; fi
+
+    cd tests/ ; names[2]="make tests" ; CC=$compiler LD=$compiler make |& tee kcc_build_2.txt ; results[2]="$?" ; process_kcc_config 2
+    
+}
+
+_test() {
+    cd dhcpcd/tests/crypt/
+    names[0]="crypt" ; ./run-test |& tee kcc_out_0.txt ; results[0]="$?" ; process_config
+    
+    cd dhcpcd/tests/eloop-bench/
+    names[1]="eloop-bench" ; ./eloop-bench |& tee kcc_out_1.txt ; results[1]="$?" ; process_config
 }
 
 init
