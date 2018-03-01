5d30fa4b2cf72551fdbdb2e066f3dce0bc3893bd
Timothy Swan <timswan@protonmail.ch>
2018-03-01 03:03:49 -0600

    Added libfiber.

---
+++ tests/libfiber/test.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+[ ! -f prepare.sh ] && wget https://raw.githubusercontent.com/runtimeverification/rv-match_testing/master/prepare.sh
+base_dir=$(pwd); cd $(dirname $BASH_SOURCE); . $base_dir/prepare.sh "$@"
+
+_dependencies() {
+    :
+}
+
+_download() {
+    git clone https://github.com/acl-dev/libfiber.git
+    cd libfiber/
+    git checkout ee97eb7d43dfc9dae1e2f70e5fe4d5dd201ee3ef
+}
+
+_build() {
+    names[0]="make"
+    cd libfiber/ ; CC=$compiler LD=$compiler make |& tee kcc_build_0.txt ; results[0]="$?" ; process_kcc_config 0
+    names[1]="make samples"
+    cd samples/  ; CC=$compiler LD=$compiler make |& tee kcc_build_1.txt ; results[1]="$?" ; process_kcc_config 1
+}
+
+_test() {
+    :
+}
+
+init
