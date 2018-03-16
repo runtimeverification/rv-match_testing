52363ec515083dfddc4095ba991197da1a330c5c
Timothy Swan <timswan@protonmail.ch>
2018-03-16 17:11:23 -0500

    Test bind9-alt with the server.

--- tests/bind9-alt/test.sh
+++ tests/bind9-alt/test.sh
@@ -33,6 +33,100 @@ _test() {
     cd bind9/
     names[0]="make unit"
     make unit |& tee kcc_out_0.txt ; results[0]="$?" ; process_config 0
+
+
+    cd bin/named/
+    f="custom_folder"
+    mkdir $f
+    cd $f
+    # 127.0.0.zone, localhost.zone, named.conf, world.zone, root.hint
+
+    echo '@                               1D IN SOA   localhost. root.localhost. (
+                                    42    ; serial (yyyymmdd##)
+                                    3H    ; refresh
+                                    15M   ; retry
+                                    1W    ; expiry
+                                    1D )  ; minimum ttl
+
+                                1D  IN  NS      localhost.
+
+1.0.0.127.in-addr.arpa.         1D  IN  PTR     localhost.' > 127.0.0.zone
+
+    echo '$TTL      86400
+$ORIGIN localhost.
+@                       1D IN SOA       @ root (
+                                        42              ; serial (d. adams)
+                                        3H              ; refresh
+                                        15M             ; retry
+                                        1W              ; expiry
+                                        1D )            ; minimum
+
+                        1D IN NS        @
+                        1D IN A         127.0.0.1' > localhost.zone
+
+    echo 'options { 
+        directory replacemeplease;
+        forwarders { 10.0.0.1; };
+        notify no;
+};
+
+zone "localhost" in {
+       type master;
+       file "localhost.zone";
+};
+
+zone "0.0.127.in-addr.arpa" in {
+        type master;
+        file "127.0.0.zone";
+};
+
+zone "world.cosmos" in {
+        type master;
+        file "world.zone";
+};
+
+zone "." in {
+        type hint;
+        file "root.hint";
+};' > named.conf
+
+    pwdesc=$(echo "\"$PWD\"" | sed 's_/_\\/_g')
+    sed -i -e "s/replacemeplease/$pwdesc/g" named.conf
+    echo "named.conf : --------------------"
+    cat named.conf
+    echo "---------------------------------"
+
+    echo '$TTL 2D
+world.cosmos. IN SOA      gateway  root.world.cosmos. (
+            2003072441  ; serial
+            1D          ; refresh
+            2H          ; retry
+            1W          ; expiry
+            2D )        ; minimum
+
+            IN NS       gateway
+            IN MX       10 sun
+
+gateway     IN A        192.168.0.1
+            IN A        192.168.1.1
+sun         IN A        192.168.0.2
+moon        IN A        192.168.0.3
+earth       IN A        192.168.1.2
+mars        IN A        192.168.1.3
+www         IN CNAME    moon' > world.zone
+
+    touch root.hint
+
+    cd ..
+    sudo ./named -c ./$f/named.conf -d 100 -g -L ./out.log &
+    for i in `seq 30`; do
+        dig @localhost sun.world.cosmos &
+    done
+    echo "did loop ======================================================================================="
+    for i in `seq 2 31`; do
+        wait %${i}
+    done
+    echo "finished========================================================================================"
 }
 
 init
