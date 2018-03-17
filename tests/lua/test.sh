542c306c6935e5d8ea4883f305b343fb7e35a0fa
Timothy Swan <timswan@protonmail.ch>
2018-03-17 12:06:42 -0500

    Trying not to escape slashes but escape colons.

--- tests/lua/test.sh
+++ tests/lua/test.sh
@@ -24,7 +24,7 @@ _download() {
 _build() {
     if [ "$compiler" == "kcc" ] || [ "$compiler" == "rvpc" ] ; then
         escapedjson=$(echo "\"$json_out\"" | sed 's_/_\\/_g')
-        escapedjson=$(echo "$escapedjson" | sed 's/:/\\:/g')
+        escapedjson=$(echo "\"$json_out\"" | sed 's/:/\\:/g')
         reportflag="CFLAGS=-fissue-report=$escapedjson"
     else
         reportflag=""
