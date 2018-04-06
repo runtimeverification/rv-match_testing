143e08c37b1791d1617cca9eeff94e2a5646064f
Timothy Swan <timswan@protonmail.ch>
2018-04-06 13:08:11 -0500

    Actually tracking added cpp-reduced info.

---
+++ tests/cpp-reduced/test.sh
@@ -0,0 +1,15 @@
+https://cpp.fail/undefined-behavior-slides.pdf
+https://godbolt.org/g/uXPxF4
+https://godbolt.org/g/Mwhuki
+https://godbolt.org/g/6Zkroj
+https://godbolt.org/g/txwo3c
+https://godbolt.org/g/iNdNQW
+https://godbolt.org/g/9ws6un
+https://godbolt.org/g/ZdT5RF
+https://godbolt.org/g/ZdT5RF
+https://godbolt.org/g/yVzLnA
+https://godbolt.org/g/eUmmm3
+https://godbolt.org/g/vstq5h
+https://godbolt.org/g/osFjWC
+https://godbolt.org/g/M1SJ1C
+https://godbolt.org/g/2nw8aL
