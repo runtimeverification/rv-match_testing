42ad25a2fe177e0c0fc65600c5776a400415c334
Timothy Swan <timswan@protonmail.ch>
2018-04-25 11:36:23 -0500

    Update example script.

--- tests/curl/rv.sh
+++ tests/curl/rv.sh
@@ -5,13 +5,31 @@ apt_install() {
 	sudo apt -yqq install $@
 }
 build_step() {
-	echo "build-[$1]" ; shift ; $@
+	execute_step "build-" $@
 }
 test_step() {
-	echo "test--[$1]" ; shift ; $@
+	execute_step "test--" $@
 }
-apt_install libssl-dev
-build_step "configure" ./configure --enable-curldebug
-build_step "make" make -j`nproc`
-build_step "make tests" make -j`nproc` tests
-test_step "runtests.pl" ./tests/runtests.pl 5
+execute_step() {
+	print_string="$1[$2]"
+	echo "=== running $print_string" ; shift 2 ; $@
+	if [ "$?" == "0" ] ; then
+		echo "=== PASS $print_string"
+	else
+		echo "=== FAIL $print_string"
+		exit 1
+	fi
+}
+configuration() {
+	# Only edit this function
+	# Runtime Verification will use its own definitions for the other functions.
+	# They are available here to allow you to test this script locally.
+	apt_install libssl-dev
+	build_step autoreconf autoreconf -i
+	build_step configure ./configure --enable-curldebug
+	build_step make make -j`nproc`
+	build_step make-tests make -j`nproc` tests
+	cd tests/
+	test_step "runtests.pl" ./runtests.pl 1
+}
+configuration
