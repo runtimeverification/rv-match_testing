a6a7a76fe08cdd545bd825a54a6fee1bdb919f43
Timothy Swan <timswan@protonmail.ch>
2018-04-25 10:30:11 -0500

    Add default functions.

--- tests/curl/rv.sh
+++ tests/curl/rv.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # git repository: https://github.com/curl/curl.git
 # git checkout  : 00cda0f9b31e45512776670201f9ec2eec095338
+apt_install() {
+	sudo apt -yqq install $@
+}
+build_step() {
+	echo "build-[$1]" ; shift ; $@
+}
+test_step() {
+	echo "test--[$1]" ; shift ; $@
+}
 apt_install libssl-dev
 build_step "configure" ./configure --enable-curldebug
 build_step "make" make -j`nproc`
